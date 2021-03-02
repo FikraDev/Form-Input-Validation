17770fb1abadfaf945e6dd1a972441fdce2adcfa
fikradev <sachel.campbell@gmail.com>
2021-03-02 18:44:38 -0500

    fixed minor error

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
+const form = document.querySelector('.formx');
+
+const feedback = document.querySelector('.feedback')
+
+const regEx = /^[a-zA-z0-9]{5,}$/;
+
+
+form.username.addEventListener('keyup', e =>{
+
+    if(regEx.test(e.target.value)){
+        form.username.setAttribute('class', 'success')
+        // feedback.textContent="success"
+    }
+    else{
+        form.username.setAttribute('class', 'error')
+        
+    }
+    // console.log(e.target.value, form.username.value)
+})
+
 
 
 
