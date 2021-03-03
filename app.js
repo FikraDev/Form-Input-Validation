4d0aeb879e7cf6d02b5c4591761fc1b59f694af2
fikradev <sachel.campbell@gmail.com>
2021-03-02 21:05:02 -0500

    added functions to check inputs

--- app.js
+++ app.js
@@ -1,24 +1,42 @@
-const form = document.querySelector('.formx');
+const form = document.querySelector('.formx'); //create a reference to the form
 
-const feedback = document.querySelector('.feedback')
+const button = document.querySelector('button');
 
-const regEx = /^[a-zA-z0-9]{5,}$/;
+const regEx = /^(?=.*[A-Z])[a-zA-Z0-9]{5,}$/;  //(?=.*[A-Z])for at least one UC letter
+
+const emailPattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 
-form.username.addEventListener('keyup', e =>{
-
+form.username.addEventListener('keyup', e =>{ //verify that each key entry produces a valid entry 
+    
     if(regEx.test(e.target.value)){
-        form.username.setAttribute('class', 'success')
-        // feedback.textContent="success"
+        form.username.setAttribute('class', 'success'); //the username input is targeted using the 'name' attribute
     }
     else{
-        form.username.setAttribute('class', 'error')
-        
+        form.username.setAttribute('class', 'error');
     }
-    // console.log(e.target.value, form.username.value)
 })
 
+button.addEventListener('click', ()=>{
+    
+    checkNull();
+    checkEmail();
+});
 
+function  checkNull(){
+    if(form.username.value==='' || form.email.value==='' || form.pwd.value===''){
+        alert('You Have and Empty Field')
+    }
+}
+
+function checkEmail(){
+if (emailPattern.test(form.email.value)){
+        return true;
+    }
+    else{
+        alert('Email Field invalid!')
+    }
+}
 
 
 
